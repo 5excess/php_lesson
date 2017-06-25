7087494f329a269ff24b7301e818f2387cd00683
5excess <janosh3@gmail.com>
2017-06-25 15:08:25 +0300

    Добавил заглавную страничку.

--- lesson1.php
+++ lesson1.php
@@ -3,14 +3,16 @@
     <head>
         <title></title>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="css/style.css" rel="stylesheet">
     </head>
     <body>
     <?php
     
-    echo '<h1>VOID</h1>'
+    echo '<h1>Hello word!</h1>'
     
     ?>
+    <form action="index.html">
+    <button type="submit">Назад</button>
+    </form>
     </body>
 </html>
